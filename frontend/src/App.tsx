e3b909b1b9ba306b0704123ee8d5786de20ca6d3
OliveSerg <oliveserg@gmail.com>
2023-11-16 12:31:45 -0500

    Remove boilplate

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,11 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 /*
-TODO: Generate tagline with LLM
-
+TODO: 
+- Generate tagline with LLM
+- Get experiences from DB
+- Get project spotlight
+- 
 */
 function App() {
 	const [count, setCount] = useState(0);
