28e0c2c9512b59c446c9a18adf99b37e7d244794
OliveSerg <oliveserg@gmail.com>
2023-11-19 12:13:35 -0500

    Change name on upload

--- backend/experiences/models/vocations.py
+++ backend/experiences/models/vocations.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from experiences.models.skills import Skill
+from uuid import uuid4
+import os
+
+def rename_image(_, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid4().hex}.{ext}'
+    return os.path.join('images/', filename)
 
 class Vocation(models.Model):
     company = models.CharField(max_length=50)
@@ -13,7 +20,7 @@ class Vocation(models.Model):
     url = models.URLField(max_length=200)
     type = models.CharField(max_length=4, choices=[("WORK", _("work")), ("VOLU", _("volunteer")), ("NONP", _("non-profit")),], default="WORK")
     skills = models.ManyToManyField(Skill)
-    logo = models.ImageField(upload_to='images/', null=True, blank=True)
+    logo = models.ImageField(upload_to=rename_image, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
