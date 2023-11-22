97ca57a7d5f347e906342bbed00be76e4166f57f
OliveSerg <oliveserg@gmail.com>
2023-11-22 14:14:44 -0500

    Remove uneeded code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 /*
@@ -10,8 +9,6 @@ TODO:
 - 
 */
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
 			<header className="">Fixed nav</header>
