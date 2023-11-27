1d063bb7a58387717b1cb9d2265bda233a779696
OliveSerg <oliveserg@gmail.com>
2023-11-26 21:33:31 -0500

    Add loading message

--- frontend/src/features/contactform/contact-form.tsx
+++ frontend/src/features/contactform/contact-form.tsx
@@ -31,7 +31,7 @@ const ContactForm = () => {
 		}
 	};
 
-	if (!isSubmitSuccessful) {
+	if (isSubmitSuccessful) {
 		return (
 			<div>
 				<div className="check-animation mb-4">
@@ -61,7 +61,30 @@ const ContactForm = () => {
 	}
 
 	if (!isSubmitting) {
-		return <p>Dispatching your digital carrier pigeon... Sit tight!</p>;
+		return (
+			<div>
+				<svg
+					className="animate-spin mx-auto mb-8 h-28 w-28 text-scarlet-700"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24">
+					<circle
+						className="opacity-25"
+						cx="12"
+						cy="12"
+						r="10"
+						stroke="currentColor"
+						stroke-width="4"></circle>
+					<path
+						className="opacity-75"
+						fill="currentColor"
+						d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+				</svg>
+				<p className="text-center">
+					Dispatching your digital carrier pigeon... Sit tight!
+				</p>
+			</div>
+		);
 	}
 
 	return (
