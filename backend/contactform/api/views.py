0281a99016bdc5c92a433d8125c7cdf8fa8b84ca
OliveSerg <oliveserg@gmail.com>
2023-11-27 10:50:10 -0500

    Remove unused class

--- backend/contactform/api/views.py
+++ backend/contactform/api/views.py
@@ -1,6 +1,5 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser 
 from rest_framework import status
 from contactform.api.serializers import ContactForm
 from django.core.mail import send_mail, BadHeaderError
