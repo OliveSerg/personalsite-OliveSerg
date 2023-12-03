32e96f832a874a4da0c709835383953a0f6ae39b
OliveSerg <oliveserg@gmail.com>
2023-12-03 12:43:19 -0500

    remove unneeded styles

--- frontend/src/features/experiences/vocation-item.tsx
+++ frontend/src/features/experiences/vocation-item.tsx
@@ -8,31 +8,20 @@ type Props = {
 	selectedId: number;
 };
 
-const classNameMap = {
-	background: {
-		VOLU: "bg-scarlet-600",
-		NONP: "bg-slate-300",
-		WORK: "bg-sunglow-600",
-	},
-};
-
 const VocationItem = ({ vocation, handleClick, selectedId }: Props) => {
 	const isSelected = selectedId === vocation.id;
 
 	return (
 		<motion.div
 			className={`cursor-pointer overflow-hidden p-6  ${
-				isSelected ? "order-last" : ""
+				isSelected ? "order-last rounded" : ""
 			}`}
 			onClick={() => handleClick([vocation.id, ...vocation.skills])}
-			transition={{ layout: { duration: 0.3 } }}
+			transition={{
+				layout: { duration: 0.5 },
+			}}
 			layout>
-			<div
-				className={
-					selectedId == undefined || isSelected
-						? ""
-						: "divide-x divide-solid"
-				}>
+			<div>
 				<motion.img
 					src={vocation.logo}
 					alt={vocation.company}
@@ -49,7 +38,7 @@ const VocationItem = ({ vocation, handleClick, selectedId }: Props) => {
 					{vocation.company} - {vocation.title}
 				</a>
 			</div>
-			<motion.div className={`mt-4 ${isSelected ? "" : "hidden"}`} layout>
+			<div className={`mt-4 ${isSelected ? "" : "hidden"}`}>
 				<p className="">
 					{vocation.city}, {vocation.country}
 				</p>
@@ -58,42 +47,8 @@ const VocationItem = ({ vocation, handleClick, selectedId }: Props) => {
 					{vocation.end_date ? vocation.end_date : "present"}
 				</p>
 				<p className="">{vocation.description}</p>
-			</motion.div>
+			</div>
 		</motion.div>
-	);
-};
-const Card = ({ value }) => {
-	const [open, setOpen] = useState(false);
-	return (
-		<>
-			open ? (
-			<motion.div
-				onClick={() => setOpen(false)}
-				className="expanded-card"
-				layoutId="expandable-card"
-				style={{ background: value }}>
-				<motion.h2
-					className="expanded-card-h"
-					layoutId="expandable-card-h">
-					Expanded {value}
-				</motion.h2>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing
-					elit.Voluptate aliquam molestiae ratione sint magnam sequi
-					fugiat ullam earum distinctio fuga iure, ad odit repudiandae
-					modi est alias ipsum aperiam.Culpa?
-				</p>
-			</motion.div>
-			) : (
-			<motion.div
-				onClick={() => setOpen(true)}
-				className="normal-card"
-				layoutId="expandable-card"
-				style={{ background: value }}>
-				<motion.h1 layoutId="expandable-card-h">{value}</motion.h1>
-			</motion.div>
-			);
-		</>
 	);
 };
 
