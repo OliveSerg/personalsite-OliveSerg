bd3e9d226ef2077dcb1e9241fb6c7e5e144582f6
OliveSerg <oliveserg@gmail.com>
2023-12-11 11:39:39 -0500

    Add default color

--- frontend/src/features/background-layers/glow.tsx
+++ frontend/src/features/background-layers/glow.tsx
@@ -3,12 +3,19 @@ type Props = {
 	left: string;
 	width: string;
 	height: string;
+	color: string;
 };
 
-const Glow = ({ top, left, width, height }: Props) => {
+const Glow = ({
+	top,
+	left,
+	width,
+	height,
+	color = "from-scarlet-600 to-sunglow-500",
+}: Props) => {
 	return (
 		<div
-			className="absolute bg-gradient-to-r -z-20 from-scarlet-600 to-sunglow-500 rounded-full blur-3xl"
+			className={`absolute bg-gradient-to-r -z-20 ${color} rounded-full blur-3xl`}
 			style={{
 				top: top,
 				left: left,
