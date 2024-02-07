29f3db69f6f170ded523a30271103d3c7e18dcb0
OliveSerg <oliveserg@gmail.com>
2024-02-07 10:52:24 -0500

    Format process for huggingface implementation

--- backend/bot_core/tests/test_text_generation.py
+++ backend/bot_core/tests/test_text_generation.py
@@ -6,7 +6,7 @@ class TextGenerationTestCase(TestCase):
         worker = TextGeneration()
         chat_history = []
         question = "Hi, how are you?"
-        response = worker.generate_response(question, chat_history).content
+        response = worker.generate_response(question, chat_history)
         self.assertIsInstance(response, str)
         self.assertNotIn("Hmm, I'm not sure.", response)
         
@@ -14,12 +14,12 @@ class TextGenerationTestCase(TestCase):
         worker = TextGeneration()
         chat_history = [("Where have you worked?", "I have worked on a charity organization's Wordpress site with custom plugins and theme configurations since May 2021. Prior to that, I have been working on a company website developed in Laravel since Sep 2019. There, I was responsible for maintaining and refactoring existing site code as well as developing new features.")]
         question = "What experience do you have with PHP?"
-        response = worker.generate_response(question, chat_history).content
+        response = worker.generate_response(question, chat_history)
         chat_history.append((question, response))
         self.assertIsInstance(response, str)
         self.assertNotIn("Hmm, I'm not sure.", response)
         next_question = "Where else have you worked?"
-        response = worker.generate_response(next_question, chat_history).content
+        response = worker.generate_response(next_question, chat_history)
         self.assertIsInstance(response, str)
         self.assertNotIn("Hmm, I'm not sure.", response)
                 
@@ -27,6 +27,6 @@ class TextGenerationTestCase(TestCase):
         worker = TextGeneration()
         chat_history = []
         question = "How long have you been a lumberjack?"
-        response = worker.generate_response(question, chat_history).content
+        response = worker.generate_response(question, chat_history)
         self.assertIsInstance(response, str)
         self.assertIn("Hmm, I'm not sure.", response)
--- backend/bot_core/text_generation.py
+++ backend/bot_core/text_generation.py
@@ -1,17 +1,21 @@
-from langchain_community.chat_models import ChatOllama
-from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.llms.huggingface_hub import HuggingFaceHub
+from langchain_community.embeddings import HuggingFaceHubEmbeddings
+from langchain_community.chat_models.huggingface import ChatHuggingFace
 from langchain_community.vectorstores.pgvector import PGVector
-from langchain.schema import AIMessage, HumanMessage, format_document
+from langchain.schema import AIMessage, HumanMessage, SystemMessage, format_document
 from langchain_core.runnables import (
+    RunnableBranch,
+    RunnableLambda,
+    RunnableParallel,
     RunnablePassthrough,
-    RunnableSerializable,
 )
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.pydantic_v1 import BaseModel, Field
-from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder, PromptTemplate
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder, PromptTemplate, BasePromptTemplate
 from django.conf import settings
-from typing import List, Tuple, Dict
+from typing import List, Tuple
 from bot_core.pgvector_settings import CONNECTION_STRING
+from operator import itemgetter
 
 REPHRASE_QUESTION_TEMPLATE = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
 Chat History:
@@ -20,14 +24,14 @@ Follow Up Input: {question}
 Standalone question:"""
 
 RESPONSE_SYSTEM_TEMPLATE = """You are a software developer interviewee. Using the provided context, answer the interviewer's question to the best of your ability. The context is related to you and your past experiences.
-Generate a concise answer for a given question based solely on the provided search results. You must only use information from the provided search results. Use an unbiased and semi-professional tone. Combine search results together into a coherent answer. Do not repeat text.
+Generate a concise answer for a given question based solely on the provided context. You must only use information from the provided search results. Use an unbiased and semi-professional tone. Combine search results together into a coherent answer. Do not repeat text.
 If there is nothing in the context relevant to the question at hand, or is irrelenvent to the interview, only say "Hmm, I'm not sure.". Do not try to make up an answer unless it is related to an interview. Do not mention anything about the context.
-Anything between the following "context" html blocks is retrieved from a knowledge bank, not part of the conversation with the user.
+Anything between the following "context" html blocks is retrieved from a knowledge bank, and not part of the conversation with the user.
 <context>
     {context}
 <context/>
 
-REMEMBER: If there is no relevant information within the context, just say "Hmm, I'm not sure." Do not try to make up an answer. Do not mention anything about the context. Anything between the preceding "context" html blocks is retrieved from a knowledge bank, not part of the conversation with the user."""
+REMEMBER: If there is nothing in the context relevant to the question at hand, or is irrelenvent to the interview, only say "Hmm, I'm not sure.". Do not try to make up an answer unless it is related to an interview."""
 
 DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template(template="{page_content}")
 
@@ -37,17 +41,28 @@ class ChatHistory(BaseModel):
     
 class TextGeneration():   
     def __init__(self):
-        self.chat_model = ChatOllama(base_url=settings.OLLAMA_BASE_URL, model=settings.OLLAMA_MODEL)
+        llm = HuggingFaceHub(
+            repo_id=settings.HUGGINGFACE_REPO_ID,
+            task="text-generation",
+            huggingfacehub_api_token=settings.HUGGINGFACEHUB_API_TOKEN,
+            model_kwargs={
+                "max_new_tokens": 512,
+                "top_k": 30,
+                "temperature": 0.1,
+                "repetition_penalty": 1.03,
+            },
+        )
+        self.chat_model = ChatHuggingFace(llm=llm)
         self.vectorstore = PGVector(
-            embedding_function=OllamaEmbeddings(base_url= settings.OLLAMA_BASE_URL, model=settings.OLLAMA_MODEL),
+            embedding_function=HuggingFaceHubEmbeddings(),
             connection_string=CONNECTION_STRING,
         )
         self.retriever = self.vectorstore.as_retriever()
         self.question_chain_prompt = PromptTemplate.from_template(REPHRASE_QUESTION_TEMPLATE)
         self.response_chain_prompt = ChatPromptTemplate.from_messages([
-            ('system', RESPONSE_SYSTEM_TEMPLATE),
+            SystemMessage(content=RESPONSE_SYSTEM_TEMPLATE),
             MessagesPlaceholder("chat_history"),
-            ('human', '{question}')
+            HumanMessage(content='{question}')
         ])
             
     def _combine_documents(self, docs, document_prompt=DEFAULT_DOCUMENT_PROMPT, document_separator="\n\n"):
@@ -61,19 +76,35 @@ class TextGeneration():
             buffer.append(AIMessage(content=ai))
         return buffer
     
-    def _build_chain(self):
-        return RunnablePassthrough.assign(
-            context=self._contextualized_question | self.retriever | self._combine_documents
-            ) | self.response_chain_prompt | self.chat_model
-        
-    def _contextualized_question(self, input: dict) -> RunnableSerializable[Dict, str] | str:
-        if input.get("chat_history"):
-            return self.question_chain_prompt | self.chat_model | StrOutputParser()
-        else:
-            return input["question"]     
+    def _build_chain(self) -> BasePromptTemplate:
+        _search_query = RunnableBranch(
+        # If input includes chat_history, we condense it with the follow-up question
+        (
+            RunnableLambda(lambda x: bool(x.get("chat_history"))).with_config(
+                run_name="HasChatHistoryCheck"
+            ),  # Condense follow-up question and chat into a standalone_question
+            RunnablePassthrough.assign(
+                chat_history=lambda x: self._format_chat_history(x["chat_history"])
+            )
+            | self.question_chain_prompt
+            | self.chat_model
+            | StrOutputParser(),
+        ),
+            # Else, we have no chat history, so just pass through the question
+            RunnableLambda(itemgetter("question")),
+        )
+
+        _inputs = RunnableParallel(
+            {
+                "question": lambda x: x["question"],
+                "chat_history": lambda x: self._format_chat_history(x["chat_history"]),
+                "context": _search_query | self.retriever | self._combine_documents,
+            }
+        ).with_types(input_type=ChatHistory)
+
+        return _inputs | self.response_chain_prompt | self.chat_model | StrOutputParser()    
             
     def generate_response(self, question: str, chat_history: List[Tuple[str,str]] = []) -> str:
         conversational_qa_chain = self._build_chain()
-        chat_history = self._format_chat_history(chat_history)
         response = conversational_qa_chain.invoke({'question': question, 'chat_history': chat_history})
         return response
